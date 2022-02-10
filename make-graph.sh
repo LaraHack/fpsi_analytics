738a196ffdb242f7862061d4567343062dd6837e
Tobias Kuhn <kuhntobias@gmail.com>
2022-02-10 07:35:24 +0100

    Make graph with colors

--- make-graph.sh
+++ make-graph.sh
@@ -4,16 +4,18 @@ set -e
 
 cat np-graph.head.dot > np-graph.dot
 
-ls sparql-results \
-  | grep -v -- -to- \
-  | awk '{print "cat sparql-results/"$1" | sed 1d"}' \
-  | bash \
-  | sed -r 's/^"([^"]*)".*$/\1/' \
-  | grep 'http://purl.org/np/' \
-  | sort \
-  | uniq \
-  | awk '{print "\""substr($1,20,10)"\" [URL=\""$1"\"]"}' \
-  >> np-graph.dot
+while read line; do
+  query=${line% *}
+  color=${line#* }
+  cat sparql-results/$query.csv \
+    | sed 1d \
+    | sed -r 's/^"([^"]*)".*$/\1/' \
+    | grep 'http://purl.org/np/' \
+    | sort \
+    | uniq \
+    | awk -v color=$color '{print "\""substr($1,20,10)"\" [fillcolor="color",URL=\""$1"\"]"}' \
+    >> np-graph.dot
+done < color-map.txt
 
 ls sparql-results \
   | grep -- -to- \
